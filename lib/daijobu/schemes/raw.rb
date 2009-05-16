7a1b2cd269de2b2c25acafe99e3aeb78f83014cc
Sander Hartlage <sander@outside.in>
2009-05-15 23:45:10 -0400

    Added Raw scheme

---
+++ lib/daijobu/schemes/raw.rb
@@ -0,0 +1,15 @@
+module Daijobu
+  module Scheme
+    class Raw
+      
+      def parse(str)
+        str
+      end
+      
+      def unparse(obj)
+        obj
+      end
+      
+    end
+  end
+end
